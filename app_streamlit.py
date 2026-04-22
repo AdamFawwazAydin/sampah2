57f542c2ccc85975641b529ae9c75ed97931a5b4
AdamFawwazAydin <aydinadamfaww@gmail.com>
2026-04-23 03:29:15 +0700

    fix streamlit requirementss

--- app_streamlit.py
+++ app_streamlit.py
@@ -5,30 +5,37 @@ from PIL import Image
 import gdown
 import os
 
+st.set_page_config(page_title="Klasifikasi Sampah")
+
 # ======================
 # DOWNLOAD MODEL
 # ======================
 MODEL_PATH = "model_sampah.h5"
 
 if not os.path.exists(MODEL_PATH):
-    st.write("Downloading model...")
-    url = "https://drive.google.com/file/d/1fs5cqFvZyXorbs6fZaxWvGOrKtFfodlb/view?usp=sharing"
+    st.info("Downloading model...")
+    
+    file_id = "1fs5cqFvZyXorbs6fZaxWvGOrKtFfodlb"
+    url = f"https://drive.google.com/file/d/1fs5cqFvZyXorbs6fZaxWvGOrKtFfodlb/view?usp=sharing"
+    
     gdown.download(url, MODEL_PATH, quiet=False)
 
 # ======================
-# LOAD MODEL
+# LOAD MODEL (SAFE)
 # ======================
-import streamlit as st
-from tensorflow.keras.models import load_model
+@st.cache_resource
+def load_ml_model():
+    from tensorflow.keras.models import load_model
+    return load_model(MODEL_PATH)
 
-model = load_model("model_sampah.h5")  # atau model.keras
+model = load_ml_model()
 
 # ======================
 # PREPROCESS
 # ======================
 def preprocess_image(image):
     img = np.array(image)
-    img = cv2.resize(img, (150,150))
+    img = cv2.resize(img, (150, 150))
     img = img / 255.0
     img = np.expand_dims(img, axis=0)
     return img
@@ -36,17 +43,17 @@ def preprocess_image(image):
 # ======================
 # UI
 # ======================
-st.title("♻️ Klasifikasi Sampah (CNN)")
+st.title("♻️ Klasifikasi Sampah CNN")
 st.write("Organik vs Anorganik")
 
 # ======================
-# UPLOAD GAMBAR
+# UPLOAD
 # ======================
-uploaded_file = st.file_uploader("Upload Gambar", type=["jpg","png","jpeg"])
+uploaded_file = st.file_uploader("Upload Gambar", type=["jpg", "png", "jpeg"])
 
-if uploaded_file is not None:
+if uploaded_file:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Gambar Upload", use_column_width=True)
+    st.image(image, caption="Gambar Upload")
 
     img = preprocess_image(image)
     pred = model.predict(img)
@@ -57,15 +64,15 @@ if uploaded_file is not None:
         st.success("Organik")
 
 # ======================
-# WEBCAM
+# CAMERA
 # ======================
-st.write("### 📷 Ambil dari Kamera")
+st.write("### 📷 Kamera")
 
 camera_image = st.camera_input("Ambil Foto")
 
-if camera_image is not None:
+if camera_image:
     image = Image.open(camera_image)
-    st.image(image, caption="Hasil Kamera", use_column_width=True)
+    st.image(image, caption="Hasil Kamera")
 
     img = preprocess_image(image)
     pred = model.predict(img)
